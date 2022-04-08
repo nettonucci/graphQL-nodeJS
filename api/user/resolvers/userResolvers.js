a82802b9b536c499bc9a2c3da0f7eb0a2dfc56b4
José Nucci Netto <nettonucci@gmail.com>
2022-04-08 18:30:38 -0300

    Create userResolvers.js

---
+++ api/user/resolvers/userResolvers.js
@@ -0,0 +1,22 @@
+const {GraphQLScalarType} = require('graphql');
+
+const userResolvers = {
+    DateTime: new GraphQLScalarType({ 
+        name: 'DateTime', 
+        description: 'String de data e hora no formate ISO-8601', 
+        serialize: (value) => value.toISOString, 
+        parseValue: (value) => new Date(value), 
+        parseLiteral: (ast) => new Date(ast.value) 
+    }),
+    Query: {
+        users: (root, args, {dataSources}) => dataSources.usersAPI.getUsers(),
+        user: (root, args, {dataSources}) => dataSources.usersAPI.getUserById(args.id)
+    },
+    Mutation: {
+        adicionaUser: async (root, user, {dataSources}) => dataSources.usersAPI.adicionaUser(user),
+        atualizaUser: async (root, novosDados, {dataSources}) => dataSources.usersAPI.atualizaUser(novosDados),
+        deletaUser: async (root, {id}, {dataSources}) => dataSources.usersAPI.deletaUser(id)
+    }
+}
+
+module.exports = userResolvers
